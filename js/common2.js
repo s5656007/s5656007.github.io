7a4107ee53964e0b631cb7b218c52c1af490a851
s5656007 <saha5656007@mail.ru>
2016-11-23 22:28:49 +0300

    Update common2.js

--- js/common2.js
+++ js/common2.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
  $('.video_popup').magnificPopup({
- type: 'iframe'})
-
+ type: 'iframe',
+ iframe: {
+ patterns: {
+   youtube: {
+     index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+     id: 'v=', // String that splits URL in a two parts, second part should be %id%
+     // Or null - full URL will be returned
+     // Or a function that should return %id%, for example:
+     // id: function(url) { return 'parsed id'; }
+     src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0' // Урл, который берется из кода iframe
+   }
+ }
+ }
  });
