17ccf080b16d853495feb518d6ba012960b80dee
s5656007 <saha5656007@mail.ru>
2016-11-24 04:10:44 +0300

    f

--- mail.php
+++ mail.php
@@ -1,52 +1,12 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
+$recepient = "youmail@ya.ru";
+$sitename = "Название сайта";
 
-//Script Foreach
-$c = true;
-if ( $method === 'POST' ) {
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+$text = trim($_GET["text"]);
 
-	$name = trim($_GET["name"]);
-	$phone = trim($_GET["phone"]);
-	$text = trim($_GET["text"]);
-
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "name" && $key != "email" && $key != "text" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-		</tr>
-		";
-	}
-}
-} else if ( $method === 'GET' ) {
-
-	$name = trim($_GET["name"]);
-	$phone = trim($_GET["phone"]);
-	$text = trim($_GET["text"]);
-
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-		</tr>
-		";
-	}
-}
-}
-
-$message = "<table style='width: 100%;'>$message</table>";
-
-function adopt($text) {
-	return '=?UTF-8?B?'.base64_encode($text).'?=';
-}
-
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
-
-mail($admin_email, adopt($form_subject), $message, $headers );
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
